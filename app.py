745285c17062ced001663b7a70ec02dda9561e2e
Davi Lucciola <davi14007@gmail.com>
2022-11-18 22:22:11 -0300

    refractoring pages

--- app.py
+++ app.py
@@ -6,10 +6,22 @@ from db.crud import *
 app = Flask(__name__)
 
 
+# Pages Endpoints
 @app.route('/', methods=['GET'])
-def index():
-    return render_template('index.html')
+def home():
+    return render_template('home.html')
 
+@app.route('/cadastrar_produto', methods=['GET'])
+def cadastrar_produto():
+    return render_template('cadastrar_produtos.html')
+
+@app.route('/produtos_cadastrados', methods=['GET'])
+def listar_produtos():
+    pass
+
+@app.route('/atualizar produto:')
+
+# Apis Endpoints
 @app.route('/register', methods=['POST'])
 def register():
     name = request.form.get('prod_name')
@@ -17,7 +29,7 @@ def register():
     if price != '' and name != '':
         if insert(Product(product=name, price=price)):
             return render_template('success.html')
-    return render_template('error.html')
+    return render_template('error.html'), 404
 
 
 if __name__ == '__main__':
